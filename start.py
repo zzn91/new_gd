8474093810957ec078b05e55dc4f47a33b2b5499
zhao <>
2019-08-13 16:24:51 +0800

    登陆

--- start.py
+++ start.py
@@ -1,5 +1,8 @@
+import json
 
-
+import requests
+from flask import request, jsonify
+from config.config import VERSION, BASE_API_URL
 from app import app
 
 
@@ -7,6 +10,20 @@ from app import app
 
 from views.response import response_bp
 app.register_blueprint(response_bp, url_prefix='')
+
+
+
+@app.after_request
+def after_request(response):
+    headers = {'Referer': 'http://127.0.0.1:8008/66635'}
+    api_url = request.base_url.split(VERSION)[-1]
+    if api_url  == '/login':
+        url = BASE_API_URL + VERSION + api_url
+        resp = requests.post(url=url, cookies=request.cookies, data=request.form,
+                             headers=headers)
+        resp_data = json.loads(resp.content)
+
+        return jsonify(resp_data)
 
 
 
