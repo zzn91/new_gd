d842a33e395e52005ae10c5be1685053bd31e823
zhao <>
2019-08-13 16:49:51 +0800

    分发接口

--- start.py
+++ start.py
@@ -17,14 +17,26 @@ app.register_blueprint(response_bp, url_prefix='')
 def after_request(response):
     headers = {'Referer': 'http://127.0.0.1:8008/66635'}
     api_url = request.base_url.split(VERSION)[-1]
-    if api_url  == '/login':
-        url = BASE_API_URL + VERSION + api_url
+    base_url = BASE_API_URL + VERSION
+    if api_url == '/login':
+        url = base_url + api_url
         resp = requests.post(url=url, cookies=request.cookies, data=request.form,
                              headers=headers)
         resp_data = json.loads(resp.content)
 
         return jsonify(resp_data)
 
+    else:
+        url = base_url + api_url
+        if request.method == 'POST':
+            resp = requests.post(url=url, cookies=request.cookies, json=request.json,
+                                 headers=headers)
+        elif request.method == 'GET':
+            resp = requests.get(url=url, cookies=request.cookies,headers=headers)
+
+        resp_data = json.loads(resp.content)
+        return jsonify(resp_data)
+    return response
 
 
 if __name__ == '__main__':
